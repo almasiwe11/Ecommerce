0d50eaaf12f439945b30f0ad1f0ba6b94d5041c5
Almas Akhmetov <awakenedduck@gmail.com>
2023-09-06 15:17:34 +0600

    home bug solved

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -2,7 +2,7 @@
 function Hero(): JSX.Element {
   return (
     <div className="font-manrope bg-hero-mobile md:bg-hero-tablet lg:bg-hero-desktop md:h-screen h-[40rem] w-screen bg-contain bg-fill md:bg-cover bg-center bg-no-repeat ">
-      <div className="flex flex-col items-center wrapper text-center lg:text-left lg:items-start text-white  mt-28 md:mt-56 px-6 lg:px-0   ">
+      <div className="flex flex-col items-center wrapper text-center lg:text-left lg:items-start text-white  pt-28 md:pt-56 px-6 lg:px-0   ">
         <p className="uppercase tracking-[8px]  text-xl text-grayish ">
           new product
         </p>
